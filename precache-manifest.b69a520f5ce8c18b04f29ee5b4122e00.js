192783fdd9ba931cd4ec1450d0b7d23f9439f216
Natalia Ilina <metlinova86@gmail.com>
2020-02-26 13:05:44 +0300

    Updates

---
+++ precache-manifest.b69a520f5ce8c18b04f29ee5b4122e00.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930fb30a5b0f24d8f17ce4463b40f791",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e807d240c665a4869f43",
+    "url": "/portfolio/static/css/main.7e15974a.chunk.css"
+  },
+  {
+    "revision": "90b920551eaa0e1886de",
+    "url": "/portfolio/static/js/2.f8dc561d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.f8dc561d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e807d240c665a4869f43",
+    "url": "/portfolio/static/js/main.8423237d.chunk.js"
+  },
+  {
+    "revision": "d89ad7dd13ac8d86cc0e",
+    "url": "/portfolio/static/js/runtime-main.94c268e8.js"
+  },
+  {
+    "revision": "9aacb4591b1ba1661b354a6dd061760f",
+    "url": "/portfolio/static/media/background.9aacb459.jpg"
+  },
+  {
+    "revision": "fa27854f670506ba839406fb538b72c7",
+    "url": "/portfolio/static/media/device.fa27854f.jpg"
+  },
+  {
+    "revision": "860f9c60945613a1eb0b3772eb1f3ed2",
+    "url": "/portfolio/static/media/github.860f9c60.svg"
+  },
+  {
+    "revision": "87b3c6c2bb626863beaecd465c618d66",
+    "url": "/portfolio/static/media/keksobooking.87b3c6c2.jpg"
+  },
+  {
+    "revision": "3c7598623af196ff5ca06699e9bed52a",
+    "url": "/portfolio/static/media/kekstagram.3c759862.jpg"
+  },
+  {
+    "revision": "3bff0e2a51f87e0eb51ceef182963b3e",
+    "url": "/portfolio/static/media/logo.3bff0e2a.png"
+  },
+  {
+    "revision": "d15de335a043b1da8f0dd098994c1df1",
+    "url": "/portfolio/static/media/mail.d15de335.svg"
+  },
+  {
+    "revision": "1e287127899829a3e42dab3b8d31b82d",
+    "url": "/portfolio/static/media/mishka.1e287127.jpg"
+  },
+  {
+    "revision": "23f31f6b3941cae2dc109f2ac8441032",
+    "url": "/portfolio/static/media/pixel-perfect.23f31f6b.jpg"
+  },
+  {
+    "revision": "5d9193c5d05433ea15129a86c8e4a4e5",
+    "url": "/portfolio/static/media/sixCities.5d9193c5.jpg"
+  },
+  {
+    "revision": "e948aa1b20721b9009351a551c24dfeb",
+    "url": "/portfolio/static/media/skype.e948aa1b.svg"
+  }
+]);
